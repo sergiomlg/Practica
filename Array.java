54f45073da594604f511e2a01c0fae6b17c6e1e3
sergiomlg <sergio.fernandez.campos@gmail.com>
2017-05-16 10:15:00 +0200

    size0

---
+++ Array.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Array {
+	
+	@Test
+	public void ArraysizeEs0() {
+		assertEquals(new Integer(0), new Integer((new ArrayAsociativo()).size()));
+	}
+	
+
+}
